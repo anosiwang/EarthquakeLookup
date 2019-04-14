51079df1f387a1e06431b171c8d534602945d3d2
Anosi's Page <anosi@outlook.com>
2019-04-15 02:31:06 +0800

    Big Changes

--- js/earthquakeCalculater.js
+++ js/earthquakeCalculater.js
@@ -12,52 +12,47 @@
 
 
 function earthquakeCal (x) {
-    var quake = new Object();
+    var quake = {
+        pga: (x * 9.81).toFixed(2),//Peak ground acceleration（m/s2)
+        seismicIntensity: "",
+        seismiCity: "" 
+    };
 
     //Calculate PGA
-    quake.pga = x * 9.81; //Peak ground acceleration（m/s2)
     
     // quake.seismiCity // Seismicity
     // quake.seismicIntensity //Seismic Intensity
     
 
     //Calculate Seismic Intensity and Seismicity
-    switch (quake.pga) {
-        case 0<=pga<0.22 : 
-            quake.seismicIntensity = "I, II, III, IIII";
-            quake.seismiCity = "<3.7";
-            break;
-        case 0.22<=pga<0.45 : 
-            quake.seismicIntensity = "V";
-            quake.seismiCity = "4.3";
-            break;
-        case 0.45<=pga<0.90 :
-            quake.seismicIntensity = "VI"; 
-            quake.seismiCity = "4.9";
-            break;
-        case 0.90<=pga<1.78 :
-            quake.seismicIntensity = "VII"; 
-            quake.seismiCity = "5.5";
-            break;
-        case 1.78<=pga<3.54 :
-            quake.seismicIntensity = "VIII"; 
-            quake.seismiCity = "6.1";
-            break;
-        case 3.54<=pga<7.08 :
-            quake.seismicIntensity = "IX"; 
-            quake.seismiCity = "6.7";
-            break;
-        case 7.08<=pga<14.15 :
-            quake.seismicIntensity = "X";
-            quake.seismiCity = "7.3";
-            break;
-        case 14.15<=pga :
-            quake.seismicIntensity = "XI, XII";
-            quake.seismiCity = ">7.9";  
-            break;
-        default: 
-            quake.seismicIntensity = "Unknown Error";
-            quake.seismiCity = "Unknown Error";
+    Number(quake.pga);
+    if (0<=quake.pga && quake.pga<0.22) {
+        quake.seismicIntensity = "I, II, III, IIII";
+        quake.seismiCity = "<3.7";
+    } else if (0.22<=quake.pga && quake.pga<0.45) {
+        quake.seismicIntensity = "V";
+        quake.seismiCity = "4.3";
+    } else if (0.45<=quake.pga && quake.pga<0.90) {
+        quake.seismicIntensity = "VI"; 
+        quake.seismiCity = "4.9";
+    } else if (0.90<=quake.pga && quake.pga<1.78) {
+        quake.seismicIntensity = "VII"; 
+        quake.seismiCity = "5.5";
+    } else if (1.78<=quake.pga && quake.pga<3.54) {
+        quake.seismicIntensity = "VIII"; 
+        quake.seismiCity = "6.1";
+    } else if (3.54<=quake.pga && quake.pga<7.08) {
+        quake.seismicIntensity = "IX"; 
+        quake.seismiCity = "6.7";
+    } else if (7.08<=quake.pga && quake.pga<14.15) {
+        quake.seismicIntensity = "X";
+        quake.seismiCity = "7.3";
+    } else if (14.15<=quake.pga) {
+        quake.seismicIntensity = "XI, XII";
+        quake.seismiCity = ">7.9";  
+    } else {
+        quake.seismicIntensity = "Unknown Error";
+        quake.seismiCity = "Unknown Error";
     };
-    return (quake);
-}
+   return (quake);
+}; 
